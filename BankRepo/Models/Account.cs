964d4720fd19dd3bed5f1ae1a7b8b629c98eb767
kalle (jag) <kalle.jillheden@gmail.com>
2018-11-07 13:47:54 +0100

    Added bank models

---
+++ BankRepo/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace BankRepo
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ BankRepo/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace BankRepo
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string OrganizationId { get; set; }
+        public string OrganizationName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+        public string Telephone { get; set; }
+    }
+}
