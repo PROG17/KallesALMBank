e1da64abd8b4642793e4af27871ec0b244852b17
kalle (jag) <kalle.jillheden@gmail.com>
2018-11-07 14:31:46 +0100

    Added unit test for verifying account lookup

--- KallesBank.Tests/BankRepoTests.cs
+++ KallesBank.Tests/BankRepoTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using BankRepo;
 using BankRepo.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -31,6 +32,20 @@ namespace KallesBank.Tests
 
                 // Assert
                 Assert.IsNotNull(customer);
+            }
+        }
+
+        [TestMethod]
+        public void AccountLookupForAllCustomers()
+        {
+            foreach (Customer customer in _repo.Customers)
+            {
+                // Act
+                List<Account> accounts = _repo.GetCustomerAccounts(customer.Id);
+                List<Account> expected = _repo.Accounts.Where(a => a.CustomerId == customer.Id).ToList();
+                
+                // Assert
+                CollectionAssert.AreEquivalent(expected, accounts);
             }
         }
     }
