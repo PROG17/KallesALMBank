0a804e3446fe58887be51b789bb9b868a8e4b627
kalle (jag) <kalle.jillheden@gmail.com>
2018-11-07 14:33:23 +0100

    Added repository to ASP.NET proj

--- KallesBank/Startup.cs
+++ KallesBank/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BankRepo;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,6 +32,7 @@ namespace KallesBank
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddSingleton<IRepository, FileRepository>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
