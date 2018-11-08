7a0f17b89ed461b9da5b822c77a90dc3081e5123
kalle (jag) <kalle.jillheden@me.com>
2018-11-08 22:00:40 +0100

    Added message to error view

--- KallesBank/Models/ErrorViewModel.cs
+++ KallesBank/Models/ErrorViewModel.cs
@@ -7,5 +7,7 @@ namespace KallesBank.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public string Message { get; set; }
     }
 }
