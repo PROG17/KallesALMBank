936b880bdf5147ff4f748bbf3683911269b21ae4
kalle (jag) <kalle.jillheden@me.com>
2018-11-08 20:59:34 +0100

    Added more Repository unit tests

--- BankRepo.Tests/RepoTests.cs
+++ BankRepo.Tests/RepoTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using BankRepo.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -14,11 +15,20 @@ namespace BankRepo.Tests
         public void TestInitialize()
         {
             _repo = new FileBankRepository();
+        }
 
+        [TestMethod]
+        public void CustomersNotNull()
+        {
             Assert.IsNotNull(_repo.Customers);
+            CollectionAssert.AllItemsAreNotNull((ICollection)_repo.Customers);
+        }
+
+        [TestMethod]
+        public void AccountsNotNull()
+        {
             Assert.IsNotNull(_repo.Accounts);
-            CollectionAssert.AllItemsAreNotNull(_repo.Customers);
-            CollectionAssert.AllItemsAreNotNull(_repo.Accounts);
+            CollectionAssert.AllItemsAreNotNull((ICollection)_repo.Accounts);
         }
 
         [TestMethod]
@@ -64,10 +74,66 @@ namespace BankRepo.Tests
                 // Act
                 List<Account> accounts = _repo.GetCustomerAccounts(customer.Id);
                 List<Account> expected = _repo.Accounts.Where(a => a.CustomerId == customer.Id).ToList();
-                
+
                 // Assert
                 CollectionAssert.AreEquivalent(expected, accounts);
             }
         }
+
+        [TestMethod]
+        public void CustomerLookupForAllAccounts()
+        {
+            foreach (Account account in _repo.Accounts)
+            {
+                // Act
+                Customer customer = _repo.GetCustomerFromAccount(account.Id);
+                Customer expected = customer != null ? _repo.Customers.Single(c => c.Id == customer.Id) : null;
+
+                // Assert
+                Assert.IsNotNull(customer);
+                Assert.AreEqual(expected, customer);
+            }
+        }
+
+        [TestMethod]
+        public void GetCustomerNonExisting()
+        {
+            Customer customer = _repo.GetCustomer(-10);
+
+            Assert.IsNull(customer);
+        }
+
+        [TestMethod]
+        public void GetCustomerExisting()
+        {
+            foreach (Customer expected in _repo.Customers)
+            {
+                Customer customer = _repo.GetCustomer(expected.Id);
+
+                Assert.IsNotNull(customer);
+                Assert.AreEqual(expected, customer);
+            }
+        }
+
+        [TestMethod]
+        public void GetAccountNonExisting()
+        {
+            Account account = _repo.GetAccount(-10);
+
+            Assert.IsNull(account);
+        }
+
+        [TestMethod]
+        public void GetAccountExisting()
+        {
+            foreach (Account expected in _repo.Accounts)
+            {
+                Account account = _repo.GetAccount(expected.Id);
+
+                Assert.IsNotNull(account);
+                Assert.AreEqual(expected, account);
+            }
+        }
+
     }
 }
